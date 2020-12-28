d76a3341467fb4bde789531a9c3a9f03097abda6
Timy Shark <timy.shark@outlook.com>
2020-12-27 23:47:59 -0800

    Initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<pre>
+    
+    This application exposes one protected API endpoint:
+
+    GET /api/private
+
+    and receives the token in an authorization header:
+
+    Authorization: Bearer token
+
+</pre>
